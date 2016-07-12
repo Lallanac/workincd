5da11f761d3479dd60b1c3e0d39e2b05ed82ed97
Lallanac <cjw1207@gmail.com>
2016-07-12 17:14:29 +0800

    HelloWorld.java
    This is a sample program...

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public HelloWorld{
+
+  public static void main(String[] args){
+    saySomething("HelloWorld");
+  }
+
+  public String saySomething(String word){
+ 
+    System.out.println("You say ：" + word);
+  
+  }
+
+}
